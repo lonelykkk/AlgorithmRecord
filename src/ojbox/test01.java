5fa12f325412e9a11e71cbfc58f474c8882d5bbd
lonelykkk <2765314967@qq.com>
2025-03-06 16:19:14 +0800

    sandbox沙箱测试

---
+++ src/ojbox/test01.java
@@ -0,0 +1,11 @@
+package ojbox;
+
+import java.util.Scanner;
+public class test01 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        System.out.println(a);
+    }
+}
