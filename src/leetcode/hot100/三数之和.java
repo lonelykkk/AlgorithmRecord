16cbf4a9d22fbb0e92a944e75ae474ca2e514ec2
lonelykkk <2765314967@qq.com>
2025-03-10 13:12:49 +0800

    15. 三数之和

---
+++ src/leetcode/hot100/三数之和.java
@@ -0,0 +1,45 @@
+package leetcode.hot100;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 15. 三数之和
+ * 提示
+ * 给你一个整数数组 nums ，判断是否存在三元组 [nums[i], nums[j], nums[k]] 满足 i != j、i != k 且 j != k ，同时还满足 nums[i] + nums[j] + nums[k] == 0 。请你返回所有和为 0 且不重复的三元组。
+ * 注意：答案中不可以包含重复的三元组。
+ *
+ * 链接：
+ * https://leetcode.cn/problems/3sum/description/?envType=study-plan-v2&envId=top-100-liked
+ */
+public class 三数之和 {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        int len = nums.length;
+        Arrays.sort(nums);
+        for (int i = 0; i < len; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int target = -nums[i];
+            int l = i + 1;
+            int r = len - 1;
+            while (l < r) {
+                int sum = nums[l] + nums[r];
+                if (sum == target) {
+                    res.add(Arrays.asList(nums[i], nums[l], nums[r]));
+                    l++;
+                    r--;
+                    while (l < r && nums[l] == nums[l - 1]) l++;
+                    while (l < r && nums[r] == nums[r + 1]) r--;
+                } else if (sum < target) {
+                    l++;
+                } else {
+                    r--;
+                }
+            }
+        }
+        return res;
+    }
+}
