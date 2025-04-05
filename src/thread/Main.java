b6e95e5dc8b036d680082488281eb2de07168e76
lonelykkk <2765314967@qq.com>
2025-04-05 18:09:20 +0800

    输入测试

---
+++ src/thread/Main.java
@@ -0,0 +1,19 @@
+package thread;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        // 注意 hasNext 和 hasNextLine 的区别
+        String line = in.nextLine();
+        String[] strArray = line.split(" ");
+        int[] nums = new int[strArray.length];
+        for (int i = 0; i < strArray.length; i++) {
+            nums[i] = Integer.parseInt(strArray[i]);
+            System.out.println(nums[i]);
+        }
+    }
+}
