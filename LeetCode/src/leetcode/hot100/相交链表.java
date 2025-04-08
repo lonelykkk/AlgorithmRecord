d7ab60d0b3a3df162749f59ca42341ff80a0631a
lonelykkk <2765314967@qq.com>
2025-04-08 21:01:15 +0800

    160. 相交链表(解法二)

--- LeetCode/src/leetcode/hot100/相交链表.java
+++ LeetCode/src/leetcode/hot100/相交链表.java
@@ -25,6 +25,26 @@ public class 相交链表 {
         return null;
     }
 
+    public ListNode getIntersectionNode2(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+        ListNode pA = headA, pB = headB;
+        while (pA != pB) {
+            if (pA == null) {
+                pA = headB;
+            } else {
+                pA = pA.next;
+            }
+            if (pB == null) {
+                pB = headA;
+            } else {
+                pB = pB.next;
+            }
+        }
+        return pA;
+    }
+
    public class ListNode {
      int val;
      ListNode next;
