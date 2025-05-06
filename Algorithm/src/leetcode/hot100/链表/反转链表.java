16be3d75c6740c79d38b7a7a245d1bdac87d9e2b
lonelykkk <2765314967@qq.com>
2025-05-06 18:56:19 +0800

    翻转链表

---
+++ Algorithm/src/leetcode/hot100/链表/反转链表.java
@@ -0,0 +1,29 @@
+package leetcode.hot100.链表;
+
+/**
+ * @author lonelykkk
+ * @email 2765314967@qq.com
+ * @date 2025/5/1 14:10
+ * @Version V1.0
+ */
+public class 反转链表 {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+}
