457f6f6209984f969271555da79a83dda79fc61a
lonelykkk <2765314967@qq.com>
2025-05-14 15:50:19 +0800

    二叉树的最近公共祖先

---
+++ Algorithm/src/leetcode/hot100/二叉树/二叉树的最近公共祖先.java
@@ -0,0 +1,38 @@
+package leetcode.hot100.二叉树;
+
+/**
+ * @author lonelykkk
+ * @email 2765314967@qq.com
+ * @date 2025/5/14 15:39
+ * @Version V1.0
+ */
+public class 二叉树的最近公共祖先 {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left == null) {
+            return right;
+        }
+        if (right == null) {
+            return left;
+        }
+        return root;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
